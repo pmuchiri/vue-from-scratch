643d4cd11f0e9a21089bf7011415b2863087f2de
Paul Muchiri <58714292+pmuchiri@users.noreply.github.com>
2020-01-08 15:04:03 -0500

    Add files via upload

---
+++ EmployeeTable.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+        </tr>
+      </tbody>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Richard Hendricks</td>
+          <td>richard@piedpiper.com</td>
+        </tr>
+        <tr>
+          <td>Bertram Gilfoyle</td>
+          <td>gilfoyle@piedpiper.com</td>
+        </tr>
+        <tr>
+          <td>Paul Muchiri</td>
+          <td>paul@piedpiper.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'employee-table',
+    props: {
+    employees: Array,
+  },
+}
+</script>
+
+<style scoped></style>

78b219d7123d2d33a9e405bc2b43a96c58237474
Paul Muchiri <58714292+pmuchiri@users.noreply.github.com>
2020-01-09 17:16:49 -0500

    Add files via upload

---
+++ List.vue
@@ -0,0 +1,41 @@
+<template>
+     <div>
+         <!-- <p>ITEM</p> -->
+        <p v-text="dropdownResult"></p>
+
+         <dropdown @update:option="optionUpdate"></dropdown>
+     </div>
+        
+    
+</template>
+
+<script>
+    import Dropdown from './Dropdown';
+
+    export default {
+        name: "List",
+
+        components: {
+            Dropdown,
+        },
+
+        data: function () {
+            return {
+                dropdownResult: '',
+            }
+        },
+
+        methods: {
+            optionUpdate: function (value) {
+                // alert(value);
+                // console.log(value);
+                this.dropdownResult = value;
+            }
+        }
+    }   
+        
+</script>
+
+<style scoped>
+
+</style>
